af750125cee82b1c9f2362c7cdf9add3847488a7
Sushil Jacob <sushjacob3@gmail.com>
2025-10-15 10:42:22 -0400

    Code to choose random word

---
+++ hangman.js
@@ -0,0 +1,4 @@
+var POSSIBLE_WORDS = ["obdurate", "verisimilitude", "defenestrate", "obsequious","dissonant", "toady", "idempotent"];
+
+var randomIndex = parseInt(Math.random()*POSSIBLE_WORDS.length);
+var word = POSSIBLE_WORDS[randomIndex];
