04d72c3e077a59c34b8e08f1c896ef9bcd6f9203
Sushil Jacob <sushjacob3@gmail.com>
2025-10-20 10:34:06 -0400

    added guess changes to js

--- hangman.js
+++ hangman.js
@@ -5,13 +5,32 @@ var word ="";
 function newGame(){
     var randomIndex = parseInt(Math.random()*POSSIBLE_WORDS.length);
     word = POSSIBLE_WORDS(randomIndex);
-
+    guesses = "";
+     updatePage();
+}
+function guessLetter(){
+    var input = document.getElementById("guess");
+    var letter = input.value;
+    guesses+=letter;
+    updatePage();
+}
+function updatePage(){
     var clueString = "";
     for(var i = 0;i<word.length;i++)
     {
+        var currentLetter = word.charAt(i);
+        if(guesses.indexOf(currentLetter)>= 0){
+            clueString += currentLetter + " ";
+        }
+        else
         clueString+= "_";
     }
     var clue = document.getElementById("clue");
     clue.innerHTML = clueString;
 
+    var guessArea = document.getElementById("guesses");
+    guessArea.innerHTML = "Guessed Letters: " + guesses;
+}
+function guessCount(){
+    
 }
