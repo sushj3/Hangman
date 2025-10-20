5b349a695c893dc0bc8897c9dd4e96dce8627d22
Sushil Jacob <sushjacob3@gmail.com>
2025-10-20 10:05:31 -0400

    new game function

--- hangman.js
+++ hangman.js
@@ -1,4 +1,17 @@
 var POSSIBLE_WORDS = ["obdurate", "verisimilitude", "defenestrate", "obsequious","dissonant", "toady", "idempotent"];
 
-var randomIndex = parseInt(Math.random()*POSSIBLE_WORDS.length);
-var word = POSSIBLE_WORDS[randomIndex];
+var word ="";
+
+function newGame(){
+    var randomIndex = parseInt(Math.random()*POSSIBLE_WORDS.length);
+    word = POSSIBLE_WORDS(randomIndex);
+
+    var clueString = "";
+    for(var i = 0;i<word.length;i++)
+    {
+        clueString+= "_";
+    }
+    var clue = document.getElementById("clue");
+    clue.innerHTML = clueString;
+
+}
