33b28e827d72fd37cb2c9a2269885e54b7a857c1
Sushil Jacob <sushjacob3@gmail.com>
2025-10-22 09:53:44 -0400

    added functionality

--- hangman.js
+++ hangman.js
@@ -1,16 +1,23 @@
 var POSSIBLE_WORDS = ["obdurate", "verisimilitude", "defenestrate", "obsequious","dissonant", "toady", "idempotent"];
 
+var MAX_GUESSES = 6;
 var word ="";
+var guesses = "";
+var guess_count = MAX_GUESSES;
 
 function newGame(){
     var randomIndex = parseInt(Math.random()*POSSIBLE_WORDS.length);
     word = POSSIBLE_WORDS(randomIndex);
     guesses = "";
+    guess_count = MAX_GUESSES;
      updatePage();
 }
 function guessLetter(){
     var input = document.getElementById("guess");
     var letter = input.value;
+    if (word.indexOf(letter) < 0){
+        guess_count--;
+    }
     guesses+=letter;
     updatePage();
 }
@@ -25,11 +32,16 @@ function updatePage(){
         else
         clueString+= "_";
     }
+    //Update clue page
     var clue = document.getElementById("clue");
     clue.innerHTML = clueString;
-
+    //update the guesses from the user
     var guessArea = document.getElementById("guesses");
     guessArea.innerHTML = "Guessed Letters: " + guesses;
+
+    //update the image
+    var image = document.getElementById("hangmanImage");
+    image.src = "images/hangman" + guess_count + ".gif";
 }
 function guessCount(){
     
